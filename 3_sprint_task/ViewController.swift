38c21b9e301bd4907e767d989a063cba34e372b0
Xiered <vnyantost@gmail.com>
2023-06-05 18:22:11 +0300

    Выполнение задания по 3 спринту

--- 3_sprint_task/ViewController.swift
+++ 3_sprint_task/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    
+    
+    
+    
+    
+    @IBOutlet weak var meanLabel: UILabel!
+   
+    
+    @IBAction func button1(_ sender: Any) {
+        counter += 1
+        meanLabel.text = "Значение счетчика - \(counter)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
